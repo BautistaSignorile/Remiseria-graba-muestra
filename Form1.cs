981066fe141d232af974e8f5184949017be179ac
BAUTISTASIGNORILE@ies21.onmicrosoft.com <BAUTISTASIGNORILE@ies21.onmicrosoft.com>
2023-09-30 14:55:25 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remiseria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable tabla;
+        DataRow datos;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            remiseria oremiseria = new remiseria();
+            tabla = oremiseria.GetData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            datos = tabla.Rows.Find(Convert.ToInt32(textBox1.Text));
+            textBox2.Text = datos["nombre"].ToString();
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            datos["chofer"] = Convert.ToInt32(textBox1.Text);
+            datos["nombre"] = textBox2.Text;
+        }
+    }
+}
---
+++ remiseria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Remiseria
+{
+    class remiseria
+    {
+        OleDbCommand comando;
+        OleDbConnection conector;
+        OleDbDataAdapter adaptador;
+        DataTable tabla;
+        public remiseria()
+        {
+            conector = new OleDbConnection("provider=microsoft.jet.oledb.4.0;data source=REMISYA.mdb");
+            comando = new OleDbCommand();
+            adaptador = new OleDbDataAdapter(comando);
+            tabla = new DataTable();
+
+            comando.Connection = conector;
+            comando.CommandType = CommandType.TableDirect;
+            comando.CommandText = "Choferes";
+
+            adaptador.Fill(tabla);
+
+            DataColumn[] vec = new DataColumn[1];
+            vec[0] = tabla.Columns["chofer"];
+            tabla.PrimaryKey = vec;
+        }
+        public DataTable GetData()
+        {
+            return tabla;
+        }
+    }   
+}
