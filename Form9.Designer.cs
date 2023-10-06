b619c2ac573ce957b32c004ff076fa88150efed2
BAUTISTASIGNORILE@ies21.onmicrosoft.com <BAUTISTASIGNORILE@ies21.onmicrosoft.com>
2023-10-06 01:13:49 -0300

    6/10/23

--- Form9.Designer.cs
+++ Form9.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NombreDelChofer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ImporteTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha del viaje sin repetir";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 53);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NombreDelChofer,
+            this.ImporteTotal});
+            this.dataGridView1.Location = new System.Drawing.Point(171, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(382, 401);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // NombreDelChofer
+            // 
+            this.NombreDelChofer.HeaderText = "Nombre del Chofer";
+            this.NombreDelChofer.Name = "NombreDelChofer";
+            this.NombreDelChofer.ReadOnly = true;
+            // 
+            // ImporteTotal
+            // 
+            this.ImporteTotal.HeaderText = "Importe Total";
+            this.ImporteTotal.Name = "ImporteTotal";
+            this.ImporteTotal.ReadOnly = true;
+            // 
+            // Form9
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(567, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form9";
             this.Text = "Form9";
+            this.Load += new System.EventHandler(this.Form9_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreDelChofer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ImporteTotal;
     }
 }
--- Form9.cs
+++ Form9.cs
@@ -16,5 +16,72 @@ namespace Remiseria
         {
             InitializeComponent();
         }
+        Viajes oViajes;
+        DataTable tViajes;
+        Chofer oChofer;
+        DataTable tChofer;
+        private void Form9_Load(object sender, EventArgs e)
+        {
+            oChofer = new Chofer();
+            tChofer = oChofer.GetDataChofer();
+            int cantidadViajes = 0;
+            oViajes = new Viajes();
+            tViajes = oViajes.GetDataViajes();
+
+            List<DateTime> fecha = new List<DateTime>();
+
+            foreach (DataRow fila in tViajes.Rows)
+            {
+                if (!fecha.Contains(Convert.ToDateTime(fila["fecha"])))
+                {
+                    fecha.Add(Convert.ToDateTime(fila["fecha"]));
+                }
+            }
+            for (int i = 0; i < fecha.Count; i++)
+            {
+                comboBox1.Items.Add(fecha[i]);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int cantidachoferes = 0;
+            int filas = tChofer.Rows.Count;
+            decimal[,] matrizDatos = new decimal[filas, 2];
+            int f = 0;
+            foreach (DataRow fila in tChofer.Rows)
+            {
+                matrizDatos[f, 0] = Convert.ToDecimal(fila["chofer"]);
+                matrizDatos[f, 1] = 0;
+                f++;
+            }
+            for (int i = 0; i < matrizDatos.GetLength(0); i++)
+            {
+                foreach (DataRow fila in tViajes.Rows)
+                {
+                    if (comboBox1.SelectedItem.ToString() == fila["fecha"].ToString())
+                    {
+                        if (matrizDatos[i, 0] == Convert.ToInt32(fila["chofer"]))
+                        {
+                            matrizDatos[i, 1] += Convert.ToDecimal(fila["importe"].ToString());
+                        }
+                    }
+                }
+            }
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < matrizDatos.GetLength(0); i++)
+            {
+                if (matrizDatos[i, 1] != 0)
+                {
+                    string nombre = oChofer.getNombre(int.Parse(matrizDatos[i, 0].ToString()));
+                    dataGridView1.Rows.Add(nombre, matrizDatos[i, 1]);
+                }
+            }
+        }
     }
 }
